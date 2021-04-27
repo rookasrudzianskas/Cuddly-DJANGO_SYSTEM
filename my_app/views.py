d7880f3a354a4fb7e2b2d07b603670e9b31120d6
Rokas <rookas.rudzenskas@gmail.com>
2021-04-27 16:10:23 +0300

    Rokas

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from bs4 import BeautifulSoup
 import requests
+from . import models
+from requests.compat import quote_plus
+
+
+BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 
 # Create your views here.
@@ -11,6 +17,11 @@ def home(request):
 
 def new_searches(request):
     search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    print(data)
     stuff_for_frontend = {
         'search': search
     }
