1fe7b39a61156e64b6c76459e639ed8187161463
Rokas <rookas.rudzenskas@gmail.com>
2021-04-27 16:38:03 +0300

    Rokas

--- my_app/views.py
+++ my_app/views.py
@@ -4,7 +4,6 @@ import requests
 from . import models
 from requests.compat import quote_plus
 
-
 BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
 BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
@@ -21,8 +20,27 @@ def new_searches(request):
     final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
     response = requests.get(final_url)
     data = response.text
-    print(data)
+    soup = BeautifulSoup(data, features='html.parser')
+    # post_titles = soup.find_all('a', {'class': 'result-title'})
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+
+    print(post_listings)
+
+    final_postings = []
+
+    for post in post_listings:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = "N/A"
+
+        final_postings.append((post_title, post_url, post_price))
+
     stuff_for_frontend = {
-        'search': search
+        'search': search,
+        'final_postings': final_postings,
     }
     return render(request, 'my_app/new_searches.html', stuff_for_frontend)
