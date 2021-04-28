98eb04a23271aa7ee2d40f1167a0bf3e9f3f28b6
Rokas <rookas.rudzenskas@gmail.com>
2021-04-28 17:14:24 +0300

    Rokas

--- my_app/views.py
+++ my_app/views.py
@@ -40,13 +40,10 @@ def new_searches(request):
         if post.find(class_='result-image').get('data-ids'):
             post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
             post_image_url = BASE_IMAGE_URL.format(post_image_id)
-            print(post_image_url)
         else:
             post_image_url = 'https://craigslist.org/images/peace.jpg'
 
-        final_postings.append((post_title, post_url, post_price))
-
-
+        final_postings.append((post_title, post_url, post_price, post_image_url))
 
     stuff_for_frontend = {
         'search': search,
